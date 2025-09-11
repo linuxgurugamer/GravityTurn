79a55b2af16ba7ef995de9ec10ff1ed3e0fed028
linuxgurugamer@gmail.com <linuxgurugamer@gmail.com>
2025-09-11 16:38:07 -0400

    Updated version file to use github for new version info

--- GravityTurn/AssemblyVersion.cs
+++ GravityTurn/AssemblyVersion.cs
@@ -5,5 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.8.2.6")]
- [assembly: AssemblyFileVersion("1.8.2.6")]
+ [assembly: AssemblyVersion("1.8.4.0")]
+ [assembly: AssemblyFileVersion("1.8.4.0")]
